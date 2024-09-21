89af0f6b00bb58d29a0dccb97d1a98023c19a3ec
kikunohana <138283495+CinnabonK@users.noreply.github.com>
2024-09-21 15:01:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -124,5 +124,5 @@ function exitRoom(roomID, ws) {
 
 // サーバーをポート 3000 で起動
 server.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 8000');
 });
